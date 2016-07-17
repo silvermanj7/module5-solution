04c6c48caacd7d5f2556fb232a9613e9d74631fb
Dena <Dena>
2016-07-17 10:13:12 -0400

    pppp

--- js/script.js
+++ js/script.js
@@ -104,7 +104,7 @@ function buildAndShowHomeHTML (categories) {
       var homeHtmlToInsertIntoMainPage=insertProperty(homeHtml,
                    "randomCategoryShortName",
                    "'"+chosenCategoryShortName+"'");
-	 insertHtml("#main-content",homeHtmlToInsertIntoMainPage;
+	 insertHtml("#main-content",homeHtmlToInsertIntoMainPage);
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to use the insertProperty function.
